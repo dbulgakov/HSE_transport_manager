d1dc8d4b93d35283d3abbd6e670511c139a2e59e
Dmitry Bulgakov <mail@dbulgakov.com>
2016-03-16 01:08:45 +0300

    Refactoring

--- HSE_transport_manager.Common/Interfaces/ITransportSchedulerService.cs
+++ HSE_transport_manager.Common/Interfaces/ITransportSchedulerService.cs
@@ -9,6 +9,6 @@ namespace HSE_transport_manager.Common.Interfaces
 {
     public interface ITransportSchedulerService
     {
-         DailyTrainSchedule GetRoute(string startingStation, string endingStation);
+         Task<DailyTrainSchedule> GetScheduleAsync(string startingStation, string endingStation);
     }
 }
