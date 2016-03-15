c22c624f3ccae6818227ca131191c1e4a617d477
Dmitry Bulgakov <mail@dbulgakov.com>
2016-03-16 01:01:13 +0300

    Implemented UberService price query

--- UberService/UberService.cs
+++ UberService/UberService.cs
@@ -1,25 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using HSE_transport_manager.Common.Interfaces;
 using HSE_transport_manager.Common.Models.TaxiData;
+using Newtonsoft.Json;
+using UberService.DTO.Response;
 
 namespace UberService
 {
     class UberService : ITransportService, ITaxiService
     {
+        private const string ApiUrl = "https://api.uber.com";
+        private const string ApiVer = "v1";
+
         private string _authKey;
+        
+        private RequestBuilder _requestBuilder; 
+
 
         public void Initialize(string authKey)
         {
             _authKey = authKey;
+            _requestBuilder = new RequestBuilder(ApiUrl, ApiVer, _authKey);
         }
 
-        public TaxiTripData GetRoute(Coordinate startingPoint, Coordinate endingPoint)
+        public async Task<TaxiTripData> GetRouteAsync(Coordinate startingPoint, Coordinate endingPoint)
         {
-            throw new NotImplementedException();
+            TaxiTripData tripData = null;
+            await Task.Factory.StartNew(() =>
+            {
+                var requestString = _requestBuilder.PriceRequest(startingPoint, endingPoint);
+                HttpClient client = new HttpClient();
+                var responseString = client.GetAsync(requestString).Result.Content.ReadAsStringAsync();
+                var deserializedResponse = JsonConvert.DeserializeObject<PriceQueryResponse>(requestString);
+                
+                var cheapestTrip = deserializedResponse.Trips.First(p => p.Low == deserializedResponse.Trips.Min(a => a.Low));
+                tripData = new TaxiTripData
+                {
+                    StartingPoint = startingPoint,
+                    FinishPoint = endingPoint,
+                    Duration = new DateTime().AddSeconds(cheapestTrip.Duration),
+                    Price = cheapestTrip.High,
+                    TransportType = Transport.Taxi
+                };
+            });
+            return tripData;
         }
     }
 }
