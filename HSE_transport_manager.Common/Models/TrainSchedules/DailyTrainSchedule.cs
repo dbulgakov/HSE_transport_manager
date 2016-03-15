6b630e32fa3ff9bf4822b93b323a1f91311c6851
Dmitry Bulgakov <mail@dbulgakov.com>
2016-03-15 22:42:22 +0300

    Implemented TrainSchedule model

---
+++ HSE_transport_manager.Common/Models/TrainSchedules/DailyTrainSchedule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSE_transport_manager.Common.Models.TrainSchedules
+{
+    class DailyTrainSchedule
+    {
+        public List<SingleTrainSchedule> ScheduledTrains { get; set; }
+    }
+}
