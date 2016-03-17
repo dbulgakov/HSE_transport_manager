7f665f6cfa3d0e3bc6be8908187b5464a92b6461
Dmitry Bulgakov <mail@dbulgakov.com>
2016-03-17 14:06:57 +0300

    Yandex schedule implementation start

---
+++ YandexScheduleService/YandexScheduleService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HSE_transport_manager.Common.Interfaces;
+using HSE_transport_manager.Common.Models.TrainSchedules;
+
+namespace YandexScheduleService
+{
+    class YandexScheduleService: ITransportService, ITransportSchedulerService
+    {
+        private const string ApiUrl = "http://api.rasp.yandex.net";
+        private const string ApiVer = "v1.0";
+
+        private string _authKey;
+
+        private RequestBuilder _requestBuilder; 
+
+        public void Initialize(string authKey)
+        {
+            _authKey = authKey;
+        }
+
+        public DailyTrainSchedule GetRoute(string startingStationCode, string endingStationCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
