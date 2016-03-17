51402dcb84367534d9b02f9579eb409a96bbc665
drogova <rogova.daria.u@gmail.com>
2016-03-17 14:11:52 +0300

    Added data models

---
+++ MSDatabaseService/Models/HSEBuildingData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSDatabaseService.Models
+{
+    class HSEBuildingData
+    {
+        public List<string> Name { get; set; }
+        public string Address { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public List<string> SubwayStation { get; set; }
+    }
+}
