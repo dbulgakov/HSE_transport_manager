a126b1bbbc730a6feb59b56d6a5a726b580d32ad
drogova <rogova.daria.u@gmail.com>
2016-03-17 14:32:01 +0300

    Added two more models

---
+++ MSDatabaseService/Models/SubwayStationData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSDatabaseService.Models
+{
+    class SubwayStationData
+    {
+        public string Name { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+}
