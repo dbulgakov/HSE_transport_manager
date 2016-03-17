8fce5d09c0c63c1a8fb39f77a48549f9dc5c604a
drogova <rogova.daria.u@gmail.com>
2016-03-17 14:17:09 +0300

    Revert "Added data models"
    This reverts commit 51402dcb84367534d9b02f9579eb409a96bbc665.

--- MSDatabaseService/DatabaseQuery.cs
+++ MSDatabaseService/DatabaseQuery.cs
@@ -3,69 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using HSE_transport_manager.Entities;
 
 namespace MSDatabaseService
 {
     class DatabaseQuery
     {
-        static private Context context = new Context();
+        public void RefreshTransportSchedule(Context context)
+        {
+            DeleteCurrentTransportSchedule(context);
 
+        }
 
-        //static void RefreshLocalTrainsSchedule(SOMETHING data)
-        //{
-        //    if (context.LocalTrainsSchedule!=null || context.LocalTrainStops!=null)
-        //        RemoveLocalTrainsSchedule();
-
-        //    foreach (var smth in data)
-        //    {
-        //        List<LocalTrainStop> listStops = new List<LocalTrainStop>();
-        //        foreach ( var smth2 in smth)
-        //            listStops.Add( 
-        //                new LocalTrainStop 
-        //                {
-        //                    Station=context.LocalTrainStations.Single(s=>s.Name==smth2.Name),
-        //                    ArrivalTime=smth2.ArrivalTime,
-        //                    ElapsedTime=????????????
-        //                });
-        //        context.LocalTrainStops.Add(listStops);
-        //        context.SaveChanges();
-
-
-        //        context.LocalTrainsSchedule.Add(
-        //            new LocalTrainSchedule
-        //            {
-        //              DepartureTime=smth.DepartureTime,
-        //              Uid=smth.Uid,
-        //              DepartureStation=context.LocalTrainStations.Single(s=> s.Name=="Одинцово"),
-        //              Stops=listStops
-        //            });
-        //        context.SaveChanges();
-        //    }
-        //}
-
-        //static void RemoveLocalTrainsSchedule()
-        //{
-        //    foreach (var train in context.LocalTrainsSchedule)
-        //        context.LocalTrainsSchedule.Remove(train);
-        //    context.SaveChanges();
-
-        //    foreach (var stop in context.LocalTrainStops)
-        //        context.LocalTrainStops.Remove(stop);
-        //    context.SaveChanges();
-        //}
-
-        //static SMTH GetRoute(string startingPoint, string finishPoint)
-        //{
-        //    if (context.Dormitories.Any(q => q.Name == startingPoint))
-        //    {
-        //        return null;
-        //    }
-        //    else if (context.HSEBuildings.Any(q => q.Name == startingPoint))
-        //    {
-        //        return null;
-        //    }
-        //    else return null;
-        //}
+        public void DeleteCurrentTransportSchedule(Context context)
+        {
+          
+        }
     }
 }
