8144a56b6d431707710d3c832de2a5b9a0372817
drogova <rogova.daria.u@gmail.com>
2016-03-17 14:22:22 +0300

    Added model

---
+++ MSDatabaseService/Models/DormitoryData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSDatabaseService.Models
+{
+    class DormitoryData
+    {
+        public List<string> Name { get; set; }
+        public string Region { get; set; }
+        public string City { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public string Address { get; set; }
+        public List<string> SubwayStation { get; set; }
+        public string LocalTrainStation { get; set; }
+        public bool ChechDubkiBus { get; set; }
+        public int From { get; set; }
+        public int To { get; set; }
+    }
+}
