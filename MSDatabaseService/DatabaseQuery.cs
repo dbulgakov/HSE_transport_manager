49697107b5fb549affa6b7c0ceb24620348910bd
drogova <rogova.daria.u@gmail.com>
2016-03-17 19:18:28 +0300

    Updated DBQuery

--- MSDatabaseService/DatabaseQuery.cs
+++ MSDatabaseService/DatabaseQuery.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HSE_transport_manager.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,15 +9,48 @@ namespace MSDatabaseService
 {
     class DatabaseQuery
     {
-        public void RefreshTransportSchedule(Context context)
-        {
-            DeleteCurrentTransportSchedule(context);
+        private Context context = new Context();
 
+        //public void RefreshTrainSchedule(DailyTrainSchedule trainSchedule)
+        //{
+        //    if (context.LocalTrainsSchedule!=null)
+        //        RemoveTrainSchedule();
+        //    var stopsList = new List<LocalTrainStop();
+        //    foreach (var train in trainSchedule.SingleTrainSchedule)
+        //    {
+        //        foreach (var stop in train.Stops)
+        //            stopsList.Add(
+        //                new LocalTrainStop
+        //                {
+        //                    Station = context.LocalTrainStations.Single(s => s.Code == stop.Code),
+        //                    ArrivalTime = stop.ArrivalTime,
+        //                    ElapsedTime = stop.ElapsedTime
+        //                });
+
+        //        context.LocalTrainStops.Add(stopsList);
+        //        context.SaveChanges();
+
+        //        context.LocalTrainsSchedule.Add(
+        //            new LocalTrainSchedule
+        //            {
+        //                DepartureTime=train.DepartureTime,
+        //                Uid=train.Uid,
+        //                DepartureStation=context.LocalTrainStations.Single( s => s.Code==train.DepartureStation),
+        //                ArrivalStation=context.LocalTrainStations.Single( s => s.Code==train.ArrivalStation),
+        //                Stops=stopsList
+        //            });
+        //        context.SaveChanges();
+        //    } 
+        //}
+
+        public void RemoveTrainSchedule()
+        {
+            foreach (var train in context.LocalTrainsSchedule)
+                context.LocalTrainsSchedule.Remove(train);
+            foreach (var stop in context.LocalTrainStops)
+                context.LocalTrainStops.Remove(stop);
         }
 
-        public void DeleteCurrentTransportSchedule(Context context)
-        {
-          
-        }
+
     }
 }
