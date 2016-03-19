7a4e16ad92ca6b9ce866f4aac3dc0e1bd313760a
Dmitry Bulgakov <mail@dbulgakov.com>
2016-03-19 20:32:52 +0300

    fix previous commit

--- HSE_transport_manager.Common/Interfaces/ITaxiService.cs
+++ HSE_transport_manager.Common/Interfaces/ITaxiService.cs
@@ -5,6 +5,7 @@ using HSE_transport_manager.Common.Models.TaxiData;
 namespace HSE_transport_manager.Common.Interfaces
 {
     public interface ITaxiService : ITransportService
+
     {
         Task<TaxiTripData> GetRouteAsync(Coordinate startingPoint, Coordinate endingPoint);
     }
