4a1c61bf74caf1f713e1bded2be83136c0673cc4
Dmitry Bulgakov <mail@dbulgakov.com>
2016-03-19 15:01:09 +0300

    Project clean-up

--- HSE_transport_manager.Common/Models/TrainSchedulesData/TrainStop.cs
+++ HSE_transport_manager.Common/Models/TrainSchedulesData/TrainStop.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HSE_transport_manager.Common.Models.TrainSchedulesData
 {
