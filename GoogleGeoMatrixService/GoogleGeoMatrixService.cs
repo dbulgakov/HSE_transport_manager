30f6b80ca112a2067d509ecbadeed24f3df94c67
Dmitry Bulgakov <mail@dbulgakov.com>
2016-03-19 16:52:10 +0300

    Partly implemented Geo matrix class

--- GoogleGeoMatrixService/GoogleGeoMatrixService.cs
+++ GoogleGeoMatrixService/GoogleGeoMatrixService.cs
@@ -4,18 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HSE_transport_manager.Common.Interfaces;
+using HSE_transport_manager.Common.Models;
 
 namespace GoogleGeoMatrixService
 {
     class GoogleGeoMatrixService : ITransportService, ITransportMonitoringService
     {
 
+        private const string ApiUrl = "https://maps.googleapis.com/maps/api";
+
+        private string _authKey;
+
+        private RequestBuilder _requestBuilder; 
+
         public void Initialize(string authKey)
         {
-            throw new NotImplementedException();
+            _authKey = authKey;
+            _requestBuilder = new RequestBuilder(ApiUrl, _authKey);
         }
 
-        public int EstimateTripTime(HSE_transport_manager.Common.Models.Coordinate startingPoint, HSE_transport_manager.Common.Models.Coordinate endingPoint)
+        public int EstimateTripTime(Coordinate startingPoint, Coordinate endingPoint)
         {
             throw new NotImplementedException();
         }
