652a675a8cd300d27ddda8a5438c25210ac8300d
Dmitry Bulgakov <mail@dbulgakov.com>
2016-03-19 16:29:27 +0300

    Implemented interface for time estimator class, started to implement GoogleGeoMatrixService classes

---
+++ GoogleGeoMatrixService/GoogleGeoMatrixService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HSE_transport_manager.Common.Interfaces;
+
+namespace GoogleGeoMatrixService
+{
+    class GoogleGeoMatrixService : ITransportService, ITransportMonitoringService
+    {
+
+        public void Initialize(string authKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int EstimateTripTime(HSE_transport_manager.Common.Models.Coordinate startingPoint, HSE_transport_manager.Common.Models.Coordinate endingPoint)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
